3988e6c17e17426be4b0515f382e9f9a25972bc4
Shan Wong <shan.wong29@gmail.com>
2019-11-17 20:29:55 +0100

    create a route for recipe submition

--- routes/recipe.js
+++ routes/recipe.js
@@ -8,7 +8,41 @@ router.get("/create", (req, res) => {
 });
 
 router.post("/update", (req, res) => {
-  res.send(req.body);
+  let ingredients = [];
+
+  if (typeof req.body.name == "string") {
+    let obj = { name: req.body.name, qty: req.body.qty, unit: req.body.unit };
+    ingredients.push(obj);
+  } else {
+    req.body.name.forEach((element, index) => {
+      let obj = {
+        name: element,
+        qty: req.body.qty[index],
+        unit: req.body.unit[index]
+      };
+      ingredients.push(obj);
+    });
+  }
+
+  // res.send(ingredients);
+
+  Recipe.create({
+    name: req.body.title,
+    ingredients,
+    dishTypes: req.body.dishType,
+    preparationTime: req.body.preparationTime,
+    preparation: [req.body.method],
+    portions: req.body.portions,
+    source: req.body.source
+  })
+    .then(() => {
+      res.send("Success");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+  // ;
 });
 
 module.exports = router;
