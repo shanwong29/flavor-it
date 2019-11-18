4a7d055e2807d4b76f9108097f74bfe145e3305a
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2019-11-18 14:38:37 +0100

    userImage upload

---
+++ config/cloudinary.js
@@ -0,0 +1,32 @@
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET
+});
+
+const storage = cloudinaryStorage({
+  cloudinary,
+  folder: "user-image", // The name of the folder in cloudinary
+  allowedFormats: ["jpg", "png"],
+  transformation: [
+    {
+      width: 250,
+      height: 250,
+      crop: "thumb",
+      gravity: "face"
+      // radius: "max",
+      // background: "rgb:174b5e" // need to figure out if there's a way to add multiple formats. otherwise this wont work
+    }
+  ],
+  // params: { resource_type: 'raw' }, => this is in case you want to upload other type of files, not just images
+  filename: function(req, res, cb) {
+    cb(null, res.originalname); // The file on cloudinary would have the same name as the original file name
+  }
+});
+
+const uploadCloud = multer({ storage });
+module.exports = uploadCloud;
