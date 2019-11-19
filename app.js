93b1ac2ac55b9e6165e7ef7588d68bb5e25a1ef6
Shan Wong <shan.wong29@gmail.com>
2019-11-19 16:12:58 +0100

    deploy on heroku

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect("mongodb://localhost/flavor-it", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/flavor-it", {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
