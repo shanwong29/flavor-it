02ebf4f8430a68d0024babf31d887699a674a767
Shan Wong <shan.wong29@gmail.com>
2020-01-02 23:16:39 +0100

    set limit to recipe image upload: can only less than 1MB

--- public/javascripts/preview-img.js
+++ public/javascripts/preview-img.js
@@ -1,4 +1,40 @@
 let previewImg = function(event) {
+  let limitinByte = 1000000;
+  let limitInMb = limitinByte / 1000000;
+
+  if (event.target.files[0].size > limitinByte) {
+    let imageInput = document.getElementById("image");
+    imageInput.parentNode.removeChild(imageInput);
+
+    let parent = document.querySelector("#image-input-div");
+
+    let warningAttr = {
+      id: "image-warning",
+      style: "color:red; background-color: white",
+      class: "mb-0 pb-1"
+    };
+
+    let inputAttr = {
+      id: "image",
+      type: "file",
+      name: "imagePath",
+      onchange: "previewImg(event)"
+    };
+
+    addElement(parent, "p", warningAttr);
+    document.getElementById(
+      "image-warning"
+    ).innerText = `* Image size should be less than ${limitInMb}MB`;
+
+    addElement(parent, "input", inputAttr);
+
+    return;
+  }
+
+  let warning = document.getElementById("image-warning");
+  if (warning) {
+    warning.parentNode.removeChild(warning);
+  }
   let output = document.querySelector(".output");
   output.src = URL.createObjectURL(event.target.files[0]);
 };
