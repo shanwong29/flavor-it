ddf08464d005476f32a7391f61f31b0a9fb0b2b8
Shan Wong <shan.wong29@gmail.com>
2020-01-02 14:08:01 +0100

    Modify model: change comment content field from String to array

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-  content: String,
+  content: [],
   author: {
     type: Schema.Types.ObjectId,
     ref: "User"
